8c780d5ef1d5abae465a46f60a22d25f55b8c554
kush001 <vaghanikush01@gmail.com>
2026-09-07 17:09:02 +0530

    update blog design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import appwriteService from "../appwrite/config";
-import { Container, PostCard } from "../components";
+import { container, PostCard } from "../components";
 
 function Home() {
     const [posts, setPosts] = useState([]);
